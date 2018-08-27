62c0845eae8730a3f0db718f6f9887dce83b4a9f
Mark J. Becker <herzog31@users.noreply.github.com>
2018-08-27 12:52:36 +0200

    Cross Platform Support (#12)

--- ci/build.js
+++ ci/build.js
@@ -43,6 +43,9 @@ if ("test-it" in pkg.scripts) {
     process.env.OW_PACKAGE_SUFFIX = `common-${branch}-${buildNum}`;
 
     try {
+        // Fix serverless permissions
+        ci.sh('$(npm bin)/lerna run fix-serverless-permission');
+
         ci.withWskCredentials(process.env.WSK_API_HOST, process.env.CORE_WSK_NAMESPACE, process.env.CORE_WSK_AUTH_STRING, () => {
             ci.sh('$(npm bin)/lerna run deploy-suffix --concurrency 1');
         });
